c1563635a49608788d43bad31e8c923f3e211d6e
shiwirockztar <68036093+shiwirockztar@users.noreply.github.com>
2020-07-10 18:14:21 -0500

    Update APP.php

--- web/config/APP.php
+++ web/config/APP.php
@@ -1,6 +1,6 @@
 <?php 
 
-	const SERVERURL="https://gentle-journey-51582.herokuapp.com/";
+	const SERVERURL="https://prestamos2000.herokuapp.com/";
 	const COMPANY="SISTEMAS DGC";
 	const MONEDA= "$";
 	date_default_timezone_set("America/Bogota");
